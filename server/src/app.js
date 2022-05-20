5dadddbaf7b592042aaa2c21260b3fec48bcbae5
sri-1237 <srilakshmi.sadanala.eagles.oc.edu>
2022-05-20 14:23:44 -0500

    initial-code

---
+++ server/src/app.js
@@ -0,0 +1,29 @@
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+//const {sequelize} = require('./models')
+//const config = require('./config/config')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+//require('./passport')
+
+//require('./routes')(app)
+
+app.get('/status',(req,res) =>
+{
+    res.send({
+        message: 'hello world!!'
+    })
+
+})
+// sequelize.sync({force: false})
+//   .then(() => {
+    app.listen(8081)
+//     console.log(`Server started on port ${config.port}`)
+//   })
